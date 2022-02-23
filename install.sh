5f717fde3731e5e8de33915bfe8846a8143c7bdb
Filip Kowalik <f.kowalik97@gmail.com>
2022-02-23 20:58:47 +0100

    add exa install

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 #!/bin/fish
 
-if ! type -q fortune
-        sudo apt install fortune-mod
-end
+# install needed packages
+sudo apt install -qy \
+fortune-mod \
+cowsay \
+unzip
 
-if ! type -q cowsay
-	sudo apt install cowsay
+# install exa
+set EXA_VERSION (curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+if ! type -q exa
+	curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v$EXA_VERSION.zip"
+	sudo unzip -q exa.zip bin/exa -d /usr/local
+	rm -rf exa.zip
+else
+	echo "exa is already installed ($EXA_VERSION)"
 end
 
 # load global variables
