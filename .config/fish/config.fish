1ce657206467280a0cedf8c43041b83024fd8a71
Filip Kowalik <fkowalik97@gmail.com>
2023-08-22 15:30:12 +0200

    show command duration after every command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,3 +11,9 @@ end
 if test -e $FISH_CONFIG_DIR/aliases/my-projects.sh
 	source $FISH_CONFIG_DIR/aliases/my-projects.sh
 end
+
+# print command duration after every command
+function printtime --on-event fish_postexec
+	echo
+	echo took (set_color yellow)(expr $CMD_DURATION / 1000)(set_color normal)s
+end
